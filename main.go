1aad9ee42627eb7823d7e7e581dc2bffb07d6235
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-07-31 11:23:22 +0200

    Added cron to go so it can run on the PC

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"pcn_stock_syncer/utils"
 
+	"github.com/go-co-op/gocron"
 	"github.com/joho/godotenv"
 )
 
@@ -15,13 +16,22 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Printf("%v Started the Script \n", time.Now().Format("2006-01-02 15:04:05"))
 
-	err = utils.HandleSyncStock()
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Starting the Cron Scheduler")
+	fmt.Println(time.Now())
 
-	fmt.Printf("%v Success \n", time.Now().Format("2006-01-02 15:04:05"))
+	s := gocron.NewScheduler(time.UTC)
+	_, _ = s.Cron("0 8,10,12,14,16 * * *").SingletonMode().Do(func() {
+		fmt.Printf("%v Started the Script \n", time.Now().Format("2006-01-02 15:04:05"))
+
+		err = utils.HandleSyncStock()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%v Success \n", time.Now().Format("2006-01-02 15:04:05"))
+
+	})
+
+	s.StartBlocking()
 
 }
