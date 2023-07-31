f211dd4053fc4ded3ca7dc002b3f0171777995b5
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-07-31 11:27:42 +0200

    Changed the time formatting a bit

--- main.go
+++ main.go
@@ -17,18 +17,17 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println("Starting the Cron Scheduler")
-	fmt.Println(time.Now())
+	fmt.Printf("%v: Started the Cron Scheduler", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
 	s := gocron.NewScheduler(time.UTC)
 	_, _ = s.Cron("0 8,10,12,14,16 * * *").SingletonMode().Do(func() {
-		fmt.Printf("%v Started the Script \n", time.Now().Format("2006-01-02 15:04:05"))
+		fmt.Printf("%v: Started the Script \n", time.Now().Format("2006-01-02 15:04:05"))
 
 		err = utils.HandleSyncStock()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%v Success \n", time.Now().Format("2006-01-02 15:04:05"))
+		fmt.Printf("%v: Success \n", time.Now().Format("2006-01-02 15:04:05"))
 
 	})
 
