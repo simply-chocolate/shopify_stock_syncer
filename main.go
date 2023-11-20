c6177bbca43c9189f538c1d6d040d24ec1585944
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-11-20 13:00:33 +0100

    removed the cron scheduler

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	fmt.Printf("%v: Started the Script \n", time.Now().Format("2006-01-02 15:04:05"))
 
 	err = utils.HandleSyncStock()
@@ -25,21 +24,4 @@ func main() {
 	}
 
 	fmt.Printf("%v: Success \n", time.Now().Format("2006-01-02 15:04:05"))
-
-	fmt.Printf("%v: Started the Cron Scheduler", time.Now().UTC().Format("2006-01-02 15:04:05"))
-
-	s := gocron.NewScheduler(time.UTC)
-	_, _ = s.Cron("0 8,10,12,14,16 * * *").SingletonMode().Do(func() {
-		fmt.Printf("%v: Started the Script \n", time.Now().Format("2006-01-02 15:04:05"))
-
-		err = utils.HandleSyncStock()
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("%v: Success \n", time.Now().Format("2006-01-02 15:04:05"))
-
-	})
-
-	s.StartBlocking()
-
 }
